5858dba5ec2662a0ad5784493b1602250dd576df
ReshabPrasad <153843058+ReshabPrasad@users.noreply.github.com>
2024-06-23 00:47:59 +0530

    Basic TS + Express project setup

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express , {Express} from 'express';
+import serverconfig from './config/serverconfig';
+ 
+const app : Express = express();
+
+app.listen(serverconfig.PORT, ()=>{
+    console.log(`Server started at PORT : ${serverconfig.PORT}`) 
+})
