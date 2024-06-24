1913fc010758df04e9f8919477919c29c8fc2e75
ReshabPrasad <153843058+ReshabPrasad@users.noreply.github.com>
2024-06-24 17:48:44 +0530

    basic setup of ioredis with bullmq

---
+++ src/config/redisconfig.ts
@@ -0,0 +1,13 @@
+import Redis from "ioredis";
+
+import ServerConfig from './serverconfig';
+
+const redisConfig = {
+    port:ServerConfig.REDIS_PORT,
+    host: ServerConfig.REDIS_HOST,
+    maxRetriesPerRequest: null
+};
+
+const redisConnection = new Redis(redisConfig);
+
+export default redisConnection;
--- src/config/serverconfig.ts
+++ src/config/serverconfig.ts
@@ -4,4 +4,6 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 3000,
+  REDIS_PORT:parseInt(process.env.PORT_REDIS || "6379",10),
+  REDIS_HOST:process.env.HOST
 };
